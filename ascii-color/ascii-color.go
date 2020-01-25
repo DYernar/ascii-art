ae612cab75a4840d5203d385bcd7b2cdf543eb1e
DYernar <eduisebai.1@gmail.com>
2020-01-25 16:56:50 +0600

    DFgdF

--- ascii-color/ascii-color.go
+++ ascii-color/ascii-color.go
@@ -5,20 +5,35 @@ import(
 	"os"
 	"strings"
 	"fmt"
+	"strconv"
 )
 
-func rgbGenerator(color string) (string,string,string) {
+func rgbGenerator(color string) (int,int,int) {
 	switch color{
-		case "black": return "0","0","0"
-		case "red": return "255","0","0"
-		case "green": return "0","255","0"
-		case "yellow": return "255","255","0"
-		case "blue": return "0","0","255"
-		case "magenta": return "255","0","255"
-		case "cyan": return "0","255","255"
-		case "orange": return "255","140","0"
+		case "black": return 0,0,0
+		case "red": return 255,0,0
+		case "green": return 0,128,0
+		case "yellow": return 255,255,0
+		case "blue": return 0,0,255
+		case "magenta": return 255,0,255
+		case "cyan": return 0,255,255
+		case "lime": return 0,255,0
+		case "silver": return 192,192,192
+		case "gray": return 128,128,128
+		case "maroon": return 128,0,0
+		case "olive": return 128,128,0
+		case "purple": return 128,0,128
+		case "teal": return 0,128,128
+		case "mint": return 170,255,195
+		case "lavender": return 230,190,255
+		case "pink": return 250,190,190
+		case "brown": return 170, 110, 40
+		case "orange": return 245, 130, 48
+		case "apricot": return 255, 215, 180
+		case "beige": return 255, 250, 200
+
 	}	
-	return "255","255","255 "
+	return 255,255,255
 }
 
 func PrintWord(word string, asciiArt []string, color string, slice string, startIndexToColor int) {
@@ -29,7 +44,7 @@ func PrintWord(word string, asciiArt []string, color string, slice string, start
 			if startIndexToColor == -1 {
 				if slice == "" {
 					r,g,b := rgbGenerator(color)
-					fmt.Printf("\033[38;2"+r+";"+g+";"+b+"m"+asciiArt[index+i])
+					fmt.Printf("\033[38;2"+strconv.Itoa(r)+";"+strconv.Itoa(g)+";"+strconv.Itoa(b)+"m"+asciiArt[index+i])
 				} else {
 					fmt.Printf("\033[38;2;255;255;255m"+asciiArt[index+i])
 				}
@@ -38,7 +53,7 @@ func PrintWord(word string, asciiArt []string, color string, slice string, start
 
 			if indexOfWord >= startIndexToColor && indexOfWord < len(slice)+startIndexToColor || startIndexToColor == 0 && slice==""{
 				r,g,b := rgbGenerator(color)
-				fmt.Printf("\033[38;2;"+r+";"+g+";"+b+"m"+asciiArt[index+i])
+				fmt.Printf("\033[38;2;"+strconv.Itoa(r)+";"+strconv.Itoa(g)+";"+strconv.Itoa(b)+"m"+asciiArt[index+i])
 			} else {
 				fmt.Printf("\033[38;2;255;255;255m"+asciiArt[index+i])
 			}
